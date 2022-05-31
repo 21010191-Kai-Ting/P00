fccb33150823f07ee8241e5aadaab6135ac7c9ce
21010191 <21010191@10.174.122.211>
2022-05-31 12:26:28 +0800

    first push to P00 repository in GitHub

---
+++ P001/src/Calculator.java
@@ -0,0 +1,22 @@
+
+//add
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	// subtract
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	// multiply
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+
+	// divide
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
